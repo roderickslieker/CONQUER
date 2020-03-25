5200715c26b814e72d9b5c825db4d6e45ef1f09b
Roderick Slieker <roderick.slieker@gmail.com>
2020-03-25 16:05:18 +0100

    Added dependency

--- R/prepare_KEGG.R
+++ R/prepare_KEGG.R
@@ -2,8 +2,9 @@
 #' @param species [[character]]
 #' @param KEGG_Type [[character]]
 #' @param keyType [[character]]
+#' @import clusterProfiler
 prepare_KEGG <- function(species, KEGG_Type="KEGG", keyType="kegg") {
-    kegg <- download_KEGG(species, KEGG_Type, keyType)
+    kegg <- clusterProfiler::download_KEGG(species, KEGG_Type, keyType)
     build_Anno(kegg$KEGGPATHID2EXTID,
                kegg$KEGGPATHID2NAME)
 }
