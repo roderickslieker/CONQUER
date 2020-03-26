904179f4ead7158adcda2eef9e1b28a52e15b002
Roderick Slieker <roderick.slieker@gmail.com>
2020-03-26 14:36:28 +0100

    Added param

--- R/build_Anno.R
+++ R/build_Anno.R
@@ -1,8 +1,10 @@
 #' This function is from the DOSE package but has not been exported. We use it directly and has therefore been added to the package
+#' @param path2gene see the DOSE pacakge
+#' @param path2name see the DOSE pacakge
 build_Anno <- function(path2gene, path2name) {
     if (!exists(".Anno_clusterProfiler_Env", envir = .GlobalEnv)) {
         pos <- 1
-        envir <- as.environment(pos) 
+        envir <- as.environment(pos)
         assign(".Anno_clusterProfiler_Env", new.env(), envir = envir)
     }
     Anno_clusterProfiler_Env <- get(".Anno_clusterProfiler_Env", envir= .GlobalEnv)
@@ -14,14 +16,14 @@ build_Anno <- function(path2gene, path2name) {
                            unlist(path2gene[[2]]))
     }
 
-    path2gene <- as.data.frame(path2gene) 
+    path2gene <- as.data.frame(path2gene)
     path2gene <- path2gene[!is.na(path2gene[,1]), ]
     path2gene <- path2gene[!is.na(path2gene[,2]), ]
     path2gene <- unique(path2gene)
-    
+
     PATHID2EXTID <- split(as.character(path2gene[,2]), as.character(path2gene[,1]))
     EXTID2PATHID <- split(as.character(path2gene[,1]), as.character(path2gene[,2]))
-    
+
     assign("PATHID2EXTID", PATHID2EXTID, envir = Anno_clusterProfiler_Env)
     assign("EXTID2PATHID", EXTID2PATHID, envir = Anno_clusterProfiler_Env)
 
@@ -33,7 +35,7 @@ build_Anno <- function(path2gene, path2name) {
         path2name <- path2name[!is.na(path2name[,2]), ]
 	path2name <- unique(path2name)
 	PATH2NAME <- as.character(path2name[,2])
-	names(PATH2NAME) <- as.character(path2name[,1]) 
+	names(PATH2NAME) <- as.character(path2name[,1])
         assign("PATHID2NAME", PATH2NAME, envir = Anno_clusterProfiler_Env)
     }
     return(Anno_clusterProfiler_Env)
