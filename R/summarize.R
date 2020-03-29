cdb653c3d0c3dba0dbf4cd2afb5964b4c4e8f1c0
Roderick Slieker <roderick.slieker@gmail.com>
2020-03-29 14:40:39 +0200

    Added example, import

--- R/summarize.R
+++ R/summarize.R
@@ -1,12 +1,19 @@
 #' Collect data for one or more SNPs and perform an optional integrative analysis on multiple tissues.
-#' @param variants [[vector]] Vector with one or more SNP names (rs*)
-#' @param multiAnalyze [[boolean]] Default is FALSE. If TRUE, an integrated analysis will be performed across the SNPs.
-#' @param tissues [[vector]] Tissues of interest. Should be a vector of names based on teh
-#' @param directory [[character]] String of the directory where the files should be stored for the SNPs investigated
-#' @param token [[character]] Token required for LDlink, which can be obtained from the LDlink website
-#' @param population [[character]] Letter code of population of interest for example " CEU"
+#' @param variants vector Vector with one or more SNP names (rs*)
+#' @param multiAnalyze Default is FALSE. If TRUE, an integrated analysis will be performed across the SNPs.
+#' @param tissues Tissues of interest. Should be a vector of names based on teh
+#' @param directory String of the directory where the files should be stored for the SNPs investigated
+#' @param token Token required for LDlink, which can be obtained from the LDlink website
+#' @param population Letter code of population of interest for example " CEU"
 #' @export
-#' @examples
+#' @examples \dontrun{
+#' library(CONQUER)
+#' summarize(variants = c("rs1558902","rs601945"),
+#' directory=getwd(),
+#' multiAnalyze=FALSE,
+#' token="sometoken",
+#' tissues=NULL)}
+
 summarize <- function(variants, multiAnalyze=FALSE, tissues ,directory=NULL, token=NULL, population="CEU") {
   # Skip if existent
   filenames <- sprintf("%s/%s.RData",directory,variants)
@@ -17,13 +24,19 @@ summarize <- function(variants, multiAnalyze=FALSE, tissues ,directory=NULL, tok
   #}
 
   # Chromatin states
-  Chromatin <- c(conquer.db::Chromatin1,conquer.db::Chromatin2,conquer.db::Chromatin3)
-  stats <- lapply(X = SNPsRemain,
-         FUN = getDataforSingleSNP,
-         directory = directory,
-         token = token,
-         population = population,
-         Chromatin=Chromatin)
+
+  if(length(SNPsRemain) != 0)
+  {
+    Chromatin <- c(conquer.db::Chromatin1,conquer.db::Chromatin2,conquer.db::Chromatin3)
+
+    stats <- lapply(X = SNPsRemain,
+                    FUN = getDataforSingleSNP,
+                    directory = directory,
+                    token = token,
+                    population = population,
+                    Chromatin=Chromatin)
+  }
+
 
   if(multiAnalyze){
     message("MultiAnalyze is true. The SNPs will be analyzed for the following tissues:")
@@ -33,6 +46,12 @@ summarize <- function(variants, multiAnalyze=FALSE, tissues ,directory=NULL, tok
     save(SNPSummary, file = paste0(directory, "/", filename))
     message(sprintf("CONQUER SNP summary saved in %s, with the following name %s", directory, filename))
   }
+  if(length(SNPsRemain) != 0)
+  {
   outputLog <- do.call(rbind,stats) %>% data.frame()
+
+  }else{
+    message("Completed, you can now run visualize.")
+  }
   return(outputLog)
 }
