0d01044b8fe0ebe83603ecb1dd7000f3c8342c61
Roderick Slieker <roderick.slieker@gmail.com>
2020-04-01 09:42:07 +0200

    Dplyr imported from

--- R/AnalyseSNPs.R
+++ R/AnalyseSNPs.R
@@ -3,7 +3,7 @@
 #' @param tissue [[character]] A character vector containing the tissues of interest.
 #' @param clustering [[character]] A character string indicating which clustring method should be used: `agnes` or `PAM`
 #' @keywords internal
-#' @import dplyr
+#' @importFrom dplyr arrange
 #' @return [[list]]
 AnalyseSNPs <- function(eQTLData, tissue, clustering){
   assign("expressionData",get(tissue, pos=2))
