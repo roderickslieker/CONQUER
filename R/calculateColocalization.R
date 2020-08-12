f3dd23bcf6052f062518e1855b5c5b81eeb40cff
Roderick Slieker <roderick.slieker@gmail.com>
2020-08-12 12:57:04 +0200

    Rename SD to SE, removed spaces

--- R/calculateColocalization.R
+++ R/calculateColocalization.R
@@ -9,7 +9,7 @@ calculateColocalization <- function(genecode.id, data.qtls=data.qtls, LD=LD)
     vid <- reshape2::colsplit(vid, "_", names=c("Chromsome","Position","REF","ALT","Build"))
     max.in <- grep("datasetId", colnames(data.in)) - 1
     tissue.eqtls <- data.in[,1:max.in]
-    
+
     newData <- lapply(colnames(tissue.eqtls), function(tissue)
     {
       #cat(tissue)
@@ -19,25 +19,25 @@ calculateColocalization <- function(genecode.id, data.qtls=data.qtls, LD=LD)
       td.sub <- td[td$Position %in% LD$start,]
       td.sub$rsID <-LD[match(td.sub$Position, LD$start),"variation"]
       td.sub <- na.omit(td.sub)
-      
+
       if(nrow(td.sub) !=0)
       {
         abf.data <- coloc::finemap.abf(dataset=list(pvalues = td.sub$pValue, beta = td.sub$nes, sdY=1,
                                                     varbeta = td.sub$se^2, snp=td.sub$rsID, type="quant"))
-        
+
         abf.data <- na.omit(abf.data)
         td.subx <- td.sub[match(abf.data$snp, td.sub$rsID),]
         abf.data$NES <- td.subx$nes
         abf.data$pValue <- td.subx$pValue
-        
-        abf.data$SD <- td.subx$se
+
+        abf.data$SE <- td.subx$se
         abf.data$Position <- td.subx$Position
-        
-        
+
+
         abf.data$Tissue <- tissue
         abf.data$genecodeId <- genecode.id
         return(abf.data)
-        
+
       }else{}
     }) %>% do.call(what=rbind)
     return(newData)
