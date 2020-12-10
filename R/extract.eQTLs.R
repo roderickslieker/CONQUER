d44034381928b2b66769e8833f7b8e15919e26f9
Roderick Slieker <roderick.slieker@gmail.com>
2020-12-10 16:35:35 +0100

    Minor fix extract eQTLs

--- R/extract.eQTLs.R
+++ R/extract.eQTLs.R
@@ -5,19 +5,39 @@
 #' @export
 extract.eQTLs <- function(directory, SNPs)
 {
-  abstractData <- lapply(SNPs,function(x){
+  extractedData <- lapply(SNPs,function(x){
     load(paste0(directory,"/",x,".RData"))
     get(x)
   })
-  #Extract QTLs from abstractData
+  #Extract QTLs from extractedData
   message("Loading eQTLs...")
-  CiseQTL <- lapply(1:length(abstractData),function(i){
-    if(nrow(abstractData[[i]]$eQTLs) != 0) return(abstractData[[i]]$eQTLs)
+  CiseQTL <- lapply(1:length(extractedData),function(i){
+    if(nrow(extractedData[[i]]$eQTLs) != 0) return(extractedData[[i]]$eQTLs)
   }) %>% do.call(what=rbind)
   CiseQTL$Type <- "Cis"
 
-  TranseQTL <- lapply(1:length(abstractData),function(i){
-    if(nrow(abstractData[[i]]$eQTLsTrans) != 0) return(abstractData[[i]]$eQTLsTrans)
+  TranseQTL <- lapply(1:length(extractedData),function(i){
+    if(nrow(extractedData[[i]]$eQTLsTrans) != 0) return(extractedData[[i]]$eQTLsTrans)
+  }) %>% do.call(what=rbind)
+  TranseQTL$Type <- "Trans"
+
+  eQTLs <- dplyr::bind_rows(CiseQTL,TranseQTL); rm(CiseQTL,TranseQTL)
+  eQTLs <- eQTLs[eQTLs$pValue <= 0.05,]
+  return(eQTLs)
+}
+
+
+extract.eQTLs.noLoad <- function(extractedData)
+{
+  #Extract QTLs from extractedData
+  message("Loading eQTLs...")
+  CiseQTL <- lapply(1:length(extractedData),function(i){
+    if(nrow(extractedData[[i]]$eQTLs) != 0) return(extractedData[[i]]$eQTLs)
+  }) %>% do.call(what=rbind)
+  CiseQTL$Type <- "Cis"
+
+  TranseQTL <- lapply(1:length(extractedData),function(i){
+    if(nrow(extractedData[[i]]$eQTLsTrans) != 0) return(extractedData[[i]]$eQTLsTrans)
   }) %>% do.call(what=rbind)
   TranseQTL$Type <- "Trans"
 
