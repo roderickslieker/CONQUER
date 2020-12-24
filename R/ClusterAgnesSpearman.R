0b3aa3b83b9ef1eb6ca411d8b9fd5e97615de303
Roderick Slieker <roderick.slieker@gmail.com>
2020-12-24 15:18:24 +0100

    Bug when low in SNPs

--- R/ClusterAgnesSpearman.R
+++ R/ClusterAgnesSpearman.R
@@ -5,8 +5,9 @@ ClusterAgnesSpearman <- function(expressionData){
   message("Performing hierarchical clustering with flexible UPGMA...")
   message("Calculating Gap statistic to determine optimal K...")
   Data <- log10(expressionData+1) %>% t() %>% scale()
+  K.max <- ifelse(nrow(Data) < 30, nrow(Data)-1, 30)
   gap_stat <- cluster::clusGap(Data, FUN = agnesPearson,
-                               K.max = 30, B = 5)
+                               K.max = K.max, B = 5)
 
   k <- cluster::maxSE(gap_stat$Tab[,3],
                       gap_stat$Tab[,4],
