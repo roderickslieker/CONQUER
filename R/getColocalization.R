51bf6ed3df6b89e325a20f98afe6ef89c7c5a29d
Roderick Slieker <roderick.slieker@gmail.com>
2021-01-05 12:00:21 +0100

    Minor update

--- R/getColocalization.R
+++ R/getColocalization.R
@@ -12,45 +12,48 @@ getColocalization <- function(rsID)
   LD <- tempdata$topHits
   #Covert names
 
-  converted <- lapply(LD$variation, function(i){
-    url <- sprintf("https://gtexportal.org/rest/v1/dataset/variant?format=json&snpId=%s&datasetId=gtex_v8", i)
-    temp <- readLines(url)
-    temp <- gsub("NaN",'"NA"',temp)
-    fromJSON(temp)[[1]]
-  }) %>% do.call(what=rbind)
+  if(nrow(LD) > 1)
+  {
+    converted <- lapply(LD$variation, function(i){
+      url <- sprintf("https://gtexportal.org/rest/v1/dataset/variant?format=json&snpId=%s&datasetId=gtex_v8", i)
+      temp <- readLines(url)
+      temp <- gsub("NaN",'"NA"',temp)
+      fromJSON(temp)[[1]]
+    }) %>% do.call(what=rbind)
 
-  LD$variantID <- converted[match(LD$variation, converted$snpId),"variantId"]
+    LD$variantID <- converted[match(LD$variation, converted$snpId),"variantId"]
 
-  eQTLs <- tempdata$eQTLs
-  eQTLs <- eQTLs[eQTLs$pValue <= 0.001,]
-  eQTLs <- eQTLs[!is.na(eQTLs$nes),]
-  all.gencode <- unique(eQTLs$gencodeId)
+    eQTLs <- tempdata$eQTLs
+    eQTLs <- eQTLs[eQTLs$pValue <= 0.001,]
+    eQTLs <- eQTLs[!is.na(eQTLs$nes),]
+    all.gencode <- unique(eQTLs$gencodeId)
 
-  lg <- length(all.gencode)
+    lg <- length(all.gencode)
 
-  if(lg >= 1){
-    data.qtls <- lapply(all.gencode, function(g){
-      # Get QTL data
-      query <- curl::curl_fetch_memory(sprintf("https://gtexportal.org/rest/v1/association/metasoft?gencodeId=%s&datasetId=gtex_v8", g))
-      chars_with_Nan <- rawToChar(query$content)
-      chars_with_NA <- gsub("NaN",'"NA"',chars_with_Nan)
-      data.qtls.temp <- jsonlite::fromJSON(chars_with_NA)[[1]]
+    if(lg >= 1){
+      data.qtls <- lapply(all.gencode, function(g){
+        # Get QTL data
+        query <- curl::curl_fetch_memory(sprintf("https://gtexportal.org/rest/v1/association/metasoft?gencodeId=%s&datasetId=gtex_v8", g))
+        chars_with_Nan <- rawToChar(query$content)
+        chars_with_NA <- gsub("NaN",'"NA"',chars_with_Nan)
+        data.qtls.temp <- jsonlite::fromJSON(chars_with_NA)[[1]]
 
-      data.qtls.temp <- data.qtls.temp[which(data.qtls.temp$variantId %in% LD$variantID),]
-      data.qtls.temp$SNP <- LD[match(data.qtls.temp$variantId, LD$variantID),1]
-      rownames(data.qtls.temp) <- NULL
-      return(data.qtls.temp)
-    })
-    names(data.qtls) <- all.gencode
+        data.qtls.temp <- data.qtls.temp[which(data.qtls.temp$variantId %in% LD$variantID),]
+        data.qtls.temp$SNP <- LD[match(data.qtls.temp$variantId, LD$variantID),1]
+        rownames(data.qtls.temp) <- NULL
+        return(data.qtls.temp)
+      })
+      names(data.qtls) <- all.gencode
 
-    allColoc <- lapply(names(data.qtls), calculateColocalization, data.qtls=data.qtls, LD=LD) %>% do.call(what=rbind)
-    allColoc$Symbol <- eQTLs[match(allColoc$genecodeId, eQTLs$gencodeId),"gene"]
-
-
-    allColoc$Lead <- ifelse(allColoc$snp == tempdata$SNP$variation, 1,NA)
-    return(allColoc)
+      allColoc <- lapply(names(data.qtls), calculateColocalization, data.qtls=data.qtls, LD=LD) %>% do.call(what=rbind)
+      allColoc$Symbol <- eQTLs[match(allColoc$genecodeId, eQTLs$gencodeId),"gene"]
 
 
+      allColoc$Lead <- ifelse(allColoc$snp == tempdata$SNP$variation, 1,NA)
+      return(allColoc)
+
+
+    }
   }
 }
 
