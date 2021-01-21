0854cc547de59daa54e4f11303ee9a6da9b5a258
Roderick Slieker <roderick.slieker@gmail.com>
2021-01-21 08:47:40 +0100

    Empty row not recognized

--- R/get_eQTL_precalculated.R
+++ R/get_eQTL_precalculated.R
@@ -11,8 +11,8 @@ get_eQTL_precalculated <- function(lead, tissues){
   data.qtls.temp <- jsonlite::fromJSON(chars_with_NA)[[1]]
 
   data.qtls.temp <- data.qtls.temp[data.qtls.temp$tissueSiteDetailId %in% tissues,]
-
-  if(length(data.qtls.temp) != 0)
+  check <- sum(length(data.qtls.temp) != 0 |  nrow(data.qtls.temp) >= 1)
+  if(check == 2)
   {
 
 
